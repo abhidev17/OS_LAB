fdf059e7fbd271a437a961f18a1b6810713fbc5c
abhidev17 <abhidevmohan3@gmail.com>
2026-03-20 19:00:41 +0530

    added SJF

--- FCFS.c
+++ FCFS.c
@@ -1,57 +1,80 @@
 #include<stdio.h>
-void main(){
- int n,i,j;
- printf("Enter the number of processes\n");
- scanf("%d",&n);
- int id[n],at[n],bt[n],ct[n],tat[n],wt[n];
- for(i=0;i<n;i++)
- {
- id[i]=i+1;
- printf("Enter arrival time and burst time of Process P%d\n",i+1);
- scanf("%d", &at[i]);
- scanf("%d", &bt[i]);
- }
- for(i=0;i<n-1;i++)
- {
- for(j=0;j<n-i-1;j++)
- {
- if(at[j]>at[j+1])
- {
- int temp=at[j];
- at[j]=at[j+1];
- at[j+1]=temp;
 
- temp=bt[j];
- bt[j]=bt[j+1];
- bt[j+1]=temp;
+int main()
+{
+    int n,i,j;
+    int at[10],bt[10],ct[10],wt[10],tat[10];
+    int time=0,temp;
 
- temp=id[j];
- id[j]=id[j+1];
- id[j+1]=temp;
- }
- }
- }
- ct[0]=at[0]+bt[0];
- for(i = 1; i < n; i++)
- {
- if(ct[i-1]<at[i])
- ct[i]=at[i]+bt[i];
- else
- ct[i]=ct[i-1]+bt[i];
- }
- float twt=0,ttat=0;
- for(i=0;i<n;i++)
- {
- tat[i]=ct[i]-at[i];
- wt[i]=tat[i]-bt[i];
- ttat=ttat+tat[i];
- twt=twt+wt[i];
- }
- printf("Process\tAT\tBT\tCT\tTAT\tWT\n");
- for(i=0;i<n;i++)
- {
- printf("P%d\t\t%d\t%d\t%d\t%d\t%d\n",id[i],at[i],bt[i],ct[i],tat[i],wt[i]);
- }
- printf("Average Turnaround Time=%f\n",ttat/n);
- printf("Average Waiting Time=%f",twt/n);
+    printf("Enter number of processes :  ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Process %d Arrival Time: ",i+1);
+        scanf("%d",&at[i]);
+
+        printf("Process %d Burst Time: ",i+1);
+        scanf("%d",&bt[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(at[i]>at[j])
+            {
+                 temp=at[i];
+                 at[i]=at[j];
+                 at[j]=temp;
+
+                temp=bt[i]; 
+                bt[i]=bt[j]; 
+                bt[j]=temp;
+            }
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        if(time<at[i])
+            time=at[i];
+
+        time+=bt[i];
+        ct[i]=time;
+
+        tat[i]=ct[i]-at[i];
+        wt[i]=tat[i]-bt[i];
+    }
+
+    printf("\nPID\tAT\tBT\tCT\tWT\tTAT\n");
+
+    for(i=0;i<n;i++)
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",i+1,at[i],bt[i],ct[i],wt[i],tat[i]);
+
+    float avgwt=0,avgtat=0;
+
+    for(i=0;i<n;i++)
+    {
+        avgwt+=wt[i];
+        avgtat+=tat[i];
+    }
+
+    printf("\nAverage WT = %.2f",avgwt/n);
+    printf("\nAverage TAT = %.2f\n",avgtat/n);
+
+    printf("\n------Gantt Chart------\n");
+
+
+    
+ for(i=0;i<n;i++){
+        printf("   P%d ",i+1);
+        }
+        printf("\n");
+ printf("0 ");
+        for(i=0;i<n;i++){
+        
+   printf("     %d",ct[i]);
+}
+   
 }
---
+++ SJF.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j;
+    int at[10],bt[10],ct[10],wt[10],tat[10],p[10];
+    int temp,time=0;
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter arrival time for Process %d: ",i+1);
+        scanf("%d",&at[i]);
+
+        printf("Enter burst time for Process %d: ",i+1);
+        scanf("%d",&bt[i]);
+
+        p[i]=i+1;
+    }
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[i]>bt[j])
+            {
+                temp=bt[i]; bt[i]=bt[j]; bt[j]=temp;
+                temp=at[i]; at[i]=at[j]; at[j]=temp;
+                temp=p[i]; p[i]=p[j]; p[j]=temp;
+            }
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        if(time<at[i])
+            time=at[i];
+
+        time+=bt[i];
+        ct[i]=time;
+
+        tat[i]=ct[i]-at[i];
+        wt[i]=tat[i]-bt[i];
+    }
+
+    printf("\nPID\tAT\tBT\tCT\tWT\tTAT\n");
+
+    for(i=0;i<n;i++)
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",p[i],at[i],bt[i],ct[i],wt[i],tat[i]);
+
+    printf("\n---------GANTT CHART---------\n");
+
+     for(i=0;i<n;i++)
+        printf("   P%d ",p[i]);
+   printf("\n");
+
+    printf("0 ");
+    for(i=0;i<n;i++)
+        printf("   %d   ",ct[i]);
+
+} 
