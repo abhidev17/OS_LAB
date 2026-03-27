921f8787e6291557432ae6afe159e08104e83909
Abhidev Mohan <abhidevmohan3@gmail.com>
2026-03-27 11:34:50 +0530

    Update SJF.c

--- scheduling_Programs/SJF.c
+++ scheduling_Programs/SJF.c
@@ -1,92 +1,73 @@
 #include<stdio.h>
-
-int main()
-{
-    int n,i,time=0,count=0;
-    int at[10],bt[10],ct[10],wt[10],tat[10],p[10];
-    int completed[10]={0};
-    float avgwt=0,avgtat=0;
-
-    printf("Enter number of processes: ");
+#include<stdlib.h>
+int main(){
+    int n,i,j;
+    int p[10],at[10],bt[10],ct[10],tat[10],wt[10];
+    int time=0,temp;
+    float avg_wt,avg_tat=0;
+    
+    printf("enter the no.of proess");
     scanf("%d",&n);
-
-    for(i=0;i<n;i++)
-    {
-        printf("Enter arrival time for Process %d: ",i+1);
-        scanf("%d",&at[i]);
-
-        printf("Enter burst time for Process %d: ",i+1);
-        scanf("%d",&bt[i]);
-
-        p[i]=i+1;
-    }
-
-    int gantt[20], gtime[20], gindex=0;
-
-    while(count < n)
-    {
-        int idx = -1;
-        int min_bt = 9999;
-
-        // find shortest job among arrived processes
-        for(i=0;i<n;i++)
-        {
-            if(at[i] <= time && completed[i]==0)
-            {
-                if(bt[i] < min_bt)
-                {
-                    min_bt = bt[i];
-                    idx = i;
-                }
-            }
-        }
-
-        // if no process has arrived yet
-        if(idx == -1)
-        {
-            time++;
-            continue;
-        }
-
-        // store Gantt chart
-        gantt[gindex] = idx;
-        gtime[gindex] = time;
-        gindex++;
-
-        time += bt[idx];
-        ct[idx] = time;
-
-        tat[idx] = ct[idx] - at[idx];
-        wt[idx] = tat[idx] - bt[idx];
-
-        avgwt += wt[idx];
-        avgtat += tat[idx];
-
-        completed[idx] = 1;
-        count++;
-    }
-
-    gtime[gindex] = time;
-
-    // 🔹 Output Table
-    printf("\nPID\tAT\tBT\tCT\tWT\tTAT\n");
-    for(i=0;i<n;i++)
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",p[i],at[i],bt[i],ct[i],wt[i],tat[i]);
-
-    printf("\nAverage WT = %.2f",avgwt/n);
-    printf("\nAverage TAT = %.2f\n",avgtat/n);
-
-    // 🔹 Gantt Chart
-    printf("\n---------GANTT CHART---------\n");
-
-    for(i=0;i<gindex;i++)
-        printf("| P%d ", gantt[i]+1);
-    printf("|\n");
-
-    for(i=0;i<=gindex;i++)
-        printf("%d   ", gtime[i]);
-
-    printf("\n");
-
-    return 0;
+     for(i=0;i<n;i++){
+         printf("enter the arrival time and burst time : \n");
+         scanf("%d%d",&at[i],&bt[i] );
+         p[i]=i+1;}
+         
+         for(i=0;i<n;i++){
+             if(time<at[i])
+             time=at[i];
+             for(j=i+1;j<n;j++){
+                 if(at[j]<=time && bt[i]>bt[j]){
+                     temp=at[i];
+                     at[i]=at[j];
+                     at[j]=temp;
+                     
+                     temp =bt[i];
+                     bt[i]=bt[j];
+                     bt[j]=temp;
+                     
+                     temp=p[i];
+                     p[i]=p[j];
+                     p[j]=temp;
+                     
+                 }
+             }
+             time=time+bt[i];
+         }
+         
+         
+     time=0;
+    
+         for(i=0;i<n;i++){
+         if(time<at[i])
+         time=at[i];
+         
+         time=time+bt[i];
+         ct[i]=time;
+         
+         tat[i]=ct[i]-at[i];
+         wt[i]=tat[i]-bt[i];
+         
+         avg_tat=avg_tat+tat[i];
+         avg_wt=avg_wt+wt[i];}
+         
+         printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+         for(i=0;i<n;i++){
+             printf("\n%d\t%d\t%d\t%d\t%d\t%d\n",p[i],at[i],bt[i],ct[i],tat[i],wt[i]);}
+             
+             printf("\navg TAT:%.2f",avg_tat/n);
+             printf("\n avg wt:%.2f",avg_wt/n);
+             
+             printf("\nGANTT CHART\n");
+             for(i=0;i<n;i++){
+                 printf("P%d\t",p[i]);
+             }
+             printf("\n");
+             printf("0\t");
+             for(i=0;i<n;i++){
+                 printf("%d\t",ct[i]);
+             }
+         
+         return 0;
+     
 }
