7c20ca6357cb9a33f5f0b2acd541b24fdc714ab9
abhidev17 <abhidevmohan3@gmail.com>
2026-03-29 23:31:10 +0530

    Add C_Scan and LOOK disk scheduling algorithms

---
+++ disk_scheduling/output/LOOK.c
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int arr[], int n) {
+    for(int i = 0; i < n; i++)
+        for(int j = i+1; j < n; j++)
+            if(arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+}
+
+int main() {
+    int n, head;
+
+    printf("Enter number of requests: ");
+    scanf("%d", &n);
+
+printf("-------Enter request within the limit (0,199)--------\n ");
+    int requests[n];
+    for(int i = 0; i < n; i++) {
+        printf("Enter request %d: ", i+1);
+        scanf("%d", &requests[i]);
+    }
+
+    printf("Enter initial head position: ");
+    scanf("%d", &head);
+
+    sort(requests, n);
+
+    int total_seek = 0, i;
+
+    for(i = 0; i < n; i++) {
+        if(head < requests[i]) break;
+    }
+
+    printf("Order: %d ", head);
+
+    // Move right
+    for(int j = i; j < n; j++) {
+        total_seek += abs(head - requests[j]);
+        head = requests[j];
+        printf("-> %d ", head);
+    }
+
+    // Move left
+    for(int j = i-1; j >= 0; j--) {
+        total_seek += abs(head - requests[j]);
+        head = requests[j];
+        printf("-> %d ", head);
+    }
+
+    printf("\nTotal seek time: %d\n", total_seek);
+    return 0;
+}
