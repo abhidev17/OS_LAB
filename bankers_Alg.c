4a957ee294194fa27d08bc5e106d67756ffa5272
Abhidev Mohan <abhidevmohan3@gmail.com>
2026-05-21 03:43:13 +0530

    Update bankers_Alg.c

--- bankers_Alg.c
+++ bankers_Alg.c
@@ -3,7 +3,7 @@
 int main()
 {
     int processes, resources, i, j, k;
-    
+
     printf("Enter number of processes: ");
     scanf("%d", &processes);
 
@@ -14,37 +14,57 @@ int main()
     int avail[10], finish[10], safeSeq[10];
 
     printf("\nEnter Allocation Matrix:\n");
-    for(i=0;i<processes;i++)
-        for(j=0;j<resources;j++)
-            scanf("%d",&alloc[i][j]);
+    for(i = 0; i < processes; i++)
+    {
+        for(j = 0; j < resources; j++)
+        {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
 
     printf("\nEnter Max Matrix:\n");
-    for(i=0;i<processes;i++)
-        for(j=0;j<resources;j++)
-            scanf("%d",&max[i][j]);
+    for(i = 0; i < processes; i++)
+    {
+        for(j = 0; j < resources; j++)
+        {
+            scanf("%d", &max[i][j]);
+        }
+    }
 
     printf("\nEnter Available Resources:\n");
-    for(i=0;i<resources;i++)
-        scanf("%d",&avail[i]);
+    for(i = 0; i < resources; i++)
+    {
+        scanf("%d", &avail[i]);
+    }
 
-    for(i=0;i<processes;i++)
-        finish[i] = 0;
-
-    for(i=0;i<processes;i++)
-        for(j=0;j<resources;j++)
+    /* Calculate Need Matrix */
+    for(i = 0; i < processes; i++)
+    {
+        for(j = 0; j < resources; j++)
+        {
             need[i][j] = max[i][j] - alloc[i][j];
+        }
+    }
+
+    /* Initialize finish array */
+    for(i = 0; i < processes; i++)
+    {
+        finish[i] = 0;
+    }
 
     int count = 0;
 
     while(count < processes)
     {
-        for(i=0;i<processes;i++)
+        int found = 0;
+
+        for(i = 0; i < processes; i++)
         {
             if(finish[i] == 0)
             {
                 int flag = 0;
 
-                for(j=0;j<resources;j++)
+                for(j = 0; j < resources; j++)
                 {
                     if(need[i][j] > avail[j])
                     {
@@ -53,25 +73,40 @@ int main()
                     }
                 }
 
+                /* Process can execute */
                 if(flag == 0)
                 {
-                    safeSeq[count++] = i;
-                    finish[i] = 1;
+                    safeSeq[count] = i;
+                    count++;
 
-                    for(k=0;k<resources;k++)
+                    finish[i] = 1;
+                    found = 1;
+
+                    for(k = 0; k < resources; k++)
+                    {
                         avail[k] += alloc[i][k];
+                    }
                 }
             }
         }
+
+        /* No process could be executed */
+        if(found == 0)
+        {
+            printf("\nSystem is NOT in safe state.\n");
+            return 0;
+        }
     }
 
-    printf("\nSafe Sequence is:\n");
+    printf("\nSystem is in SAFE state.\n");
+    printf("Safe Sequence is:\n");
 
-    for(i=0;i<processes;i++)
+    for(i = 0; i < processes; i++)
+    {
         printf("P%d ", safeSeq[i]);
+    }
 
     printf("\n");
 
     return 0;
-      
 }
